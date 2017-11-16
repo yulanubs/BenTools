bc8e7a91d09deffb679a90cdb248568a75e0a809
yulanubs <yulanubs123@qq.com>
2017-11-16 16:58:51 +0800

    修改版本

---
+++ BenTools/src/main/java/org/bentools/utils/security/MainActivity.java
@@ -0,0 +1,16 @@
+package org.bentools.utils.security;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.bentools.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
